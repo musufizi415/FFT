7eef67a4cd3da798736b5b0fce924a2a94abdb81
fu <83001754+musufizi415@users.noreply.github.com>
2021-07-04 14:41:43 +0900

    for文を周期生を使って削減

--- FFT.py
+++ FFT.py
@@ -6,18 +6,37 @@ data = [0] * point_num
 ##データの並び替えを行う（偶数と奇数に分ける）
 
 data_order = [0] * point_num
-stage = int(np.log2(point_num))
-for k in range(stage) :
+level = int(np.log2(point_num))
+for k in range(level) :
     
+    m = 0
+    big_cycle = point_num/ 2**(k + 1)
+    small_cycle = 2**k
+
+    for n in range(int(big_cycle)) :
+        #for i in range(small_cycle) :
+        
+        m += small_cycle
+        
+        for i in range(small_cycle) :
+            data_order[m] = data_order[m] + int(big_cycle)
+            m += 1
+
+
+for k in range(point_num): #作った順番にデータを並び替え
+    data[k] = data[data_order[k]]
+    print(data_order[k])
+"""
+###バタフライ演算部
+stage = int(np.log2(point_num))
+for i in range(stage):
+
     m = 0
     for n in range(int(point_num/ 2**(k + 1))) :
         for i in range(2**k) :
             m += 1
         
         for i in range(2**k) :
-            data_order[m] = data_order[m] + int(point_num / 2**(k + 1))
+            data_approxi
             m += 1
-
-
-for k in range(point_num): #作った順番にデータを並び替え
-    data[k] = data[data_order[k]]
+"""
